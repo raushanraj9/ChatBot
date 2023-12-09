82a447f2345e5e8ed92fc2c8def69d5765acb215
raushanraj9 <raushanr363@gmail.com>
2023-12-09 11:32:37 +0530

    Remove redundant code

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,9 @@ io.on("connection", (socket) => {
 });
 
 app.get("/conversation", async (req, res) => {
-  const allMessages = await MessageModel.find().limit(20).sort({ timestamp: -1 });
+  const allMessages = await MessageModel.find()
+    .limit(20)
+    .sort({ timestamp: -1 });
   return res.json({
     status: 200,
     data: {
@@ -64,74 +66,23 @@ app.get("/conversation", async (req, res) => {
   });
 });
 
-// API for conversation
-app.post("/conversation", async (req, res) => {
-  const message = req.body.message;
-
-  if (!message) {
-    return res.status(400).json({
-      status: 400,
-      message: "Message cannot be empty!",
-    });
-  }
-
-  const newMessage = new MessageModel({
-    role: "user",
-    content: message,
-  });
-  const newMessageResp = await newMessage.save();
-
-  return res.json({
-    status: 200,
-    data: {
-      message: newMessageResp,
-    },
-  });
-
-  // Calling the OpenAI API to complete the message
-  try {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: "user", content: message }],
-      model: "gpt-3.5-turbo",
-    });
-    if (
-      Array.isArray(chatCompletion.choices) &&
-      chatCompletion.choices.length > 0
-    ) {
-      return res.status(200).json({
-        status: 200,
-        data: {
-          content: chatCompletion.choices[0]?.message?.content ?? "No reply",
-        },
-      });
-    } else {
-      return res.status(500).json({
-        status: 500,
-        message: "No reply",
-      });
-    }
-  } catch (err) {
-    return res.status(500).json({
-      status: 500,
-      message: "Internal Server Error",
-    });
-  }
-});
-
 const getInTouchWithOpenAI = async (role, messageString) => {
-  let previousMessages = await MessageModel.find().limit(3).skip(1).sort({ timestamp: -1 });
-  previousMessages = previousMessages.map( (previousMessage) => ({
-	role: previousMessage.role,
-	content: previousMessage.content,
-  } ) );
+  let previousMessages = await MessageModel.find()
+    .limit(3)
+    .skip(1)
+    .sort({ timestamp: -1 });
+  previousMessages = previousMessages.map((previousMessage) => ({
+    role: previousMessage.role,
+    content: previousMessage.content,
+  }));
 
   // Calling the OpenAI API to complete the message
   try {
     const chatCompletion = await openai.chat.completions.create({
-      messages: [ ...previousMessages, { role, content: messageString }],
+      messages: [...previousMessages, { role, content: messageString }],
       model: "gpt-3.5-turbo",
     });
-    console.log("open ai result", chatCompletion.choices[0]);
+
     if (
       Array.isArray(chatCompletion.choices) &&
       chatCompletion.choices.length > 0
@@ -146,7 +97,6 @@ const getInTouchWithOpenAI = async (role, messageString) => {
       io.emit("assistant-error", "Something went wrong");
     }
   } catch (err) {
-    console.log("error ---->", err);
     io.emit("assistant-error", "Something went wrong");
   }
 };
